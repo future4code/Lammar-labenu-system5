cd8565179272a514ab92a312474e5268ba53d90d
Rafael Castilho de Castro <raphaeldavant@hotmail.com>
2022-12-17 14:18:06 -0300

    Fix migrations.ts

---
+++ src/database/BaseDB.ts
@@ -0,0 +1,27 @@
+import knex from "knex"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+export abstract class BaseDB {
+
+    public static tableEstudante = "LabSys_estudante"
+    public static tableDocente = "LabSys_docente"
+    public static tableTurma = "LabSys_turma"
+    public static tableEstudanteHobby = "LabSys_estudanteHobby"
+    public static tableHobbies = "LabSys_hobbies"
+    public static tableDocenteEspecialidades = "LabSys_docenteEspec"
+    public static tableEspecialidades = "LabSys_especialidades"
+
+    public static connection = knex({
+        client: "mysql",
+        connection: {
+            host: process.env.DB_HOST,
+            port: 3306,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE,
+            multipleStatements: true
+        },
+    })
+}
--- src/database/migrations.ts
+++ src/database/migrations.ts
@@ -1,66 +1,58 @@
-import { connection } from "./connection"
+import { BaseDB } from "./BaseDB"
 import especialidades from "./especialidades.json"
 
 const printError = (error: any) => { console.log(error.sqlMessage || error.message) }
 
-const tableEstudante = "LabSys_estudante"
-const tableDocente = "LabSys_docente"
-const tableTurma = "LabSys_turma"
-const tableEstudanteHobby = "LabSys_estudanteHobby"
-const tableHobbies = "LabSys_hobbies"
-const tableDocenteEspecialidades = "LabSys_docenteEspec"
-const tableEspecialidades = "LabSys_especialidades"
-
 const createTables = () =>
 
     // Tabela Hobbies
-    connection.schema.hasTable(tableHobbies).then((exists) => {
+    BaseDB.connection.schema.hasTable(BaseDB.tableHobbies).then((exists) => {
         if (!exists) {
-            return connection.schema
-                .createTable(tableHobbies, (table) => {
+            return BaseDB.connection.schema
+                .createTable(BaseDB.tableHobbies, (table) => {
                     table.string('id')
                     table.primary(['id'])
                     table.string('nome').notNullable().unique()
                 })
                 .then(() => {
-                    console.log(`Table ${tableHobbies} created.`)
+                    console.log(`Table ${BaseDB.tableHobbies} created.`)
                 })
         } else {
-            console.log(`Table ${tableHobbies} already exists.`)
+            console.log(`Table ${BaseDB.tableHobbies} already exists.`)
         }
     })
         .catch(printError)
 
 
 // Tabela de Relação Estudante-Hobbie
-connection.schema.hasTable(tableEstudanteHobby).then((exists) => {
+BaseDB.connection.schema.hasTable(BaseDB.tableEstudanteHobby).then((exists) => {
     if (!exists) {
-        return connection.schema
-            .createTable(tableEstudanteHobby, (table) => {
+        return BaseDB.connection.schema
+            .createTable(BaseDB.tableEstudanteHobby, (table) => {
                 table.string('id')
                 table.primary(['id'])
                 table.string('id_hobby')
-                table.foreign('id_hobby').references(`${tableHobbies}.id`)
+                table.foreign('id_hobby').references(`${BaseDB.tableHobbies}.id`)
                 table.string('id_estudante')
                 table.foreign('id_estudante')
-                    .references(`${tableEstudante}.id`)
+                    .references(`${BaseDB.tableEstudante}.id`)
                     .onDelete('cascade')
             })
             .then(() => {
-                console.log(`Table ${tableEstudanteHobby} created.`)
+                console.log(`Table ${BaseDB.tableEstudanteHobby} created.`)
             })
     } else {
-        console.log(`Table ${tableEstudanteHobby} already exists.`)
+        console.log(`Table ${BaseDB.tableEstudanteHobby} already exists.`)
     }
 })
     .catch(printError)
 
 
 // Tabela Estudantes
-connection.schema.hasTable(tableEstudante).then((exists) => {
+BaseDB.connection.schema.hasTable(BaseDB.tableEstudante).then((exists) => {
     if (!exists) {
-        return connection.schema
-            .createTable(tableEstudante, (table) => {
+        return BaseDB.connection.schema
+            .createTable(BaseDB.tableEstudante, (table) => {
                 table.string('id')
                 table.primary(['id'])
                 table.string('nome').notNullable()
@@ -68,44 +60,44 @@ connection.schema.hasTable(tableEstudante).then((exists) => {
                 table.date('data_nasc').notNullable()
                 table.string('turma_id').notNullable()
                 table.foreign('turma_id')
-                .references(`${tableTurma}.id`)
+                .references(`${BaseDB.tableTurma}.id`)
                 .onDelete('cascade')
             })
             .then(() => {
-                console.log(`Table ${tableEstudante} created.`)
+                console.log(`Table ${BaseDB.tableEstudante} created.`)
             })
     } else {
-        console.log(`Table ${tableEstudante} already exists.`)
+        console.log(`Table ${BaseDB.tableEstudante} already exists.`)
     }
 })
     .catch(printError)
 
 
 // Tabela Turma
-connection.schema.hasTable(tableTurma).then((exists) => {
+BaseDB.connection.schema.hasTable(BaseDB.tableTurma).then((exists) => {
     if (!exists) {
-        return connection.schema
-            .createTable(tableTurma, (table) => {
+        return BaseDB.connection.schema
+            .createTable(BaseDB.tableTurma, (table) => {
                 table.string('id')
                 table.primary(['id'])
                 table.string('nome').notNullable()
                 table.string('modulo').notNullable().defaultTo(0)
             })
             .then(() => {
-                console.log(`Table ${tableTurma} created.`)
+                console.log(`Table ${BaseDB.tableTurma} created.`)
             })
     } else {
-        console.log(`Table ${tableTurma} already exists.`)
+        console.log(`Table ${BaseDB.tableTurma} already exists.`)
     }
 })
     .catch(printError)
 
 
 // Tabela Docente
-connection.schema.hasTable(tableDocente).then((exists) => {
+BaseDB.connection.schema.hasTable(BaseDB.tableDocente).then((exists) => {
     if (!exists) {
-        return connection.schema
-            .createTable(tableDocente, (table) => {
+        return BaseDB.connection.schema
+            .createTable(BaseDB.tableDocente, (table) => {
                 table.string('id')
                 table.primary(['id'])
                 table.string('nome').notNullable()
@@ -113,69 +105,69 @@ connection.schema.hasTable(tableDocente).then((exists) => {
                 table.date('data_nasc').notNullable()
                 table.string('turma_id').notNullable()
                 table.foreign('turma_id')
-                .references(`${tableTurma}.id`)
+                .references(`${BaseDB.tableTurma}.id`)
                 .onDelete('cascade')
             })
             .then(() => {
-                console.log(`Table ${tableDocente} created.`)
+                console.log(`Table ${BaseDB.tableDocente} created.`)
             })
     } else {
-        console.log(`Table ${tableDocente} already exists.`)
+        console.log(`Table ${BaseDB.tableDocente} already exists.`)
     }
 })
     .catch(printError)
 
 
 // Tabela de Relação Docente-Especialidades
-connection.schema.hasTable(tableDocenteEspecialidades).then((exists) => {
+BaseDB.connection.schema.hasTable(BaseDB.tableDocenteEspecialidades).then((exists) => {
     if (!exists) {
-        return connection.schema
-            .createTable(tableDocenteEspecialidades, (table) => {
+        return BaseDB.connection.schema
+            .createTable(BaseDB.tableDocenteEspecialidades, (table) => {
                 table.string('id')
                 table.primary(['id'])
                 table.string('id_docente').notNullable()
-                table.foreign('id_docente').references(`${tableDocente}.id`)
+                table.foreign('id_docente').references(`${BaseDB.tableDocente}.id`)
                 table.string('id_especialidade').notNullable()
                 table.foreign('id_especialidade')
-                .references(`${tableEspecialidades}.id`)
+                .references(`${BaseDB.tableEspecialidades}.id`)
                 .onDelete('cascade')
             })
             .then(() => {
-                console.log(`Table ${tableDocenteEspecialidades} created.`)
+                console.log(`Table ${BaseDB.tableDocenteEspecialidades} created.`)
             })
     } else {
-        console.log(`Table ${tableDocenteEspecialidades} already exists.`)
+        console.log(`Table ${BaseDB.tableDocenteEspecialidades} already exists.`)
     }
 })
     .catch(printError)
 
 
 // Tabela Especialidades
-connection.schema.hasTable(tableEspecialidades).then((exists) => {
+BaseDB.connection.schema.hasTable(BaseDB.tableEspecialidades).then((exists) => {
     if (!exists) {
-        return connection.schema
-            .createTable(tableEspecialidades, (table) => {
+        return BaseDB.connection.schema
+            .createTable(BaseDB.tableEspecialidades, (table) => {
                 table.string('id')
                 table.primary(['id'])
                 table.string('nome').notNullable()
             })
             .then(() => {
-                console.log(`Table ${tableEspecialidades} created.`)
+                console.log(`Table ${BaseDB.tableEspecialidades} created.`)
             })
     } else {
-        console.log(`Table ${tableEspecialidades} already exists.`)
+        console.log(`Table ${BaseDB.tableEspecialidades} already exists.`)
     }
 })
     .catch(printError)
 
 
-const addEspecialidades = () => connection(tableEspecialidades)
+const addEspecialidades = () => BaseDB.connection(BaseDB.tableEspecialidades)
     .insert(especialidades)
-    .then(() => { console.log("Especialidades adicionadas.") })
+    .then(() => { console.log(`Especialidades adicionadas na tablela ${BaseDB.tableEspecialidades}.`) })
     .catch(printError)
 
-const closeConnection = () => { connection.destroy() }
+const connection = () => { BaseDB.connection.destroy() }
 
 createTables()
     .then(addEspecialidades)
-    .finally(closeConnection)
+    .finally(connection)
